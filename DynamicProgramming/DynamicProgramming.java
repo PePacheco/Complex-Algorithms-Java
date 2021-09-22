86502323e94826b1262b172837a07f88d2634b24
PePacheco <pedro_pacheco98@outlook.com>
2021-09-22 09:03:40 -0300

    Finishing the recursive method with memory

--- DynamicProgramming/DynamicProgramming.java
+++ DynamicProgramming/DynamicProgramming.java
@@ -8,14 +8,17 @@ package DynamicProgramming;
 
 import java.util.HashMap;
 
+import javax.print.DocFlavor.STRING;
+
 class DynamicProgramming {
 	static HashMap<String, Integer> memory = new HashMap<>();
 	public static void main(String[] args) {
-		System.out.println(recursive(args[0], args[0].length(), 0));
-		System.out.println(recursiveWithMemory(args[0], args[0].length(), 0));
+		System.out.println("Recursivo: " + recursive(args[0], args[0].length(), 0, true));
+		System.out.println("Com memória: " + recursiveWithMemory(args[0], args[0].length(), 0, true));
+		// System.out.println("Iterando: " + iterating(args[0], args[0].length(), 0));
 	}
 
-	public static int recursive(String entry, int entryLength, int e1) {
+	public static int recursive(String entry, int entryLength, int e1, boolean wasNot3Jump) {
 
 		if (e1 >= entryLength) {
 			return 0;
@@ -31,12 +34,15 @@ class DynamicProgramming {
 
 		int res = 0;
 
-		res = recursive(entry, entryLength, e1 + 1) + recursive(entry, entryLength, e1 + 2) + recursive(entry, entryLength, e1 + 3);
+		res = recursive(entry, entryLength, e1 + 1, true) + recursive(entry, entryLength, e1 + 2, true);
+		if (wasNot3Jump) {
+			res += recursive(entry, entryLength, e1 + 3, false);
+		}
 		
 		return res;
 	}
 
-	public static int recursiveWithMemory(String entry, int entryLength, int e1) {
+	public static int recursiveWithMemory(String entry, int entryLength, int e1, Boolean wasNot3Jump) {
 
 		if (e1 >= entryLength) {
 			return 0;
@@ -50,15 +56,18 @@ class DynamicProgramming {
 			return 1;
 		}
 
-		if (memory.containsKey(entry.substring(0, e1))) {
-			return memory.get(entry.substring(0, e1));
+		if (memory.containsKey(entry.substring(0, e1) + String.valueOf(wasNot3Jump))) {
+			return memory.get(entry.substring(0, e1) + String.valueOf(wasNot3Jump));
 		}
 
 		int res = 0;
 
-		res = recursive(entry, entryLength, e1 + 1) + recursive(entry, entryLength, e1 + 2) + recursive(entry, entryLength, e1 + 3);
+		res = recursiveWithMemory(entry, entryLength, e1 + 1, true) + recursiveWithMemory(entry, entryLength, e1 + 2, true);
+		if (wasNot3Jump) {
+			res += recursiveWithMemory(entry, entryLength, e1 + 3, false);
+		}
 
-		memory.put(entry.substring(0, e1), res);
+		memory.put(entry.substring(0, e1) + String.valueOf(wasNot3Jump), res);
 		
 		return res;
 	}
